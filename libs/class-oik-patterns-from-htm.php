d6ba5197710c072de186062c345486d0b543abd0
Herb Miller <herb@bobbingwide.com>
2021-04-29 19:17:37 +0100

    Issue #2 - Add logic to load patterns from the thisis theme's .htm files

---
+++ libs/class-oik-patterns-from-htm.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Class OIK_Patterns_From_Htm
+ * @copyright (C) Copyright Bobbing Wide 2021
+ * @package oik-patterns
+ */
+
+
+class OIK_Patterns_From_Htm {
+
+	private $pattern_name = null; // theme/filename
+	private $pattern_properties = [];
+	private $filename = null ; // eg 404-template.htm
+	private $themes = null;
+	private $theme = 'thisis';
+	private $files = [];
+
+	function __construct() {
+
+	}
+
+	/**
+	 * Hardcoded at present.
+	 */
+	function list_themes() {
+		$this->themes = ['thisis'];
+	}
+
+	function register_patterns() {
+		$this->list_themes();
+		foreach ( $this->themes as $this->theme ) {
+			$this->register_block_pattern_category();
+			$this->list_files();
+			foreach ( $this->files as $file ) {
+				$this->file = $file;
+				$this->filename = basename( $file );
+				$this->register_pattern();
+			}
+		}
+
+	}
+
+	function list_files() {
+		$theme_dir_root = dirname( get_stylesheet_directory() );
+		$mask = $theme_dir_root . '/' . $this->theme .'/block-template-parts/*.htm';
+		$this->files = glob( $mask );
+		bw_trace2( $this->files, $mask );
+	}
+
+	/**
+	 * Pattern properties consists of
+	 *
+	 * field | value
+	 * ----- | -----
+	 * title | The title of the pattern
+	 * content | block HTML markup for the pattern
+	 * description  optional |
+	 * categories optional |
+	 * keywords optional |
+	 * viewportWidth optional |
+	 */
+	function load_pattern() {
+		$this->pattern_name =  $this->theme . '/' . $this->filename;
+		$this->pattern_properties = [];
+		$this->pattern_properties['title'] = $this->filename;
+		$content = file_get_contents( $this->file );
+		if ( $content === false ) {
+			gob();
+		}
+		bw_trace2( $content, $this->file  );
+		$this->pattern_properties['content'] = $content;
+		$this->pattern_properties['categories'] = [ $this->theme ];
+	}
+
+	function register_block_pattern_category() {
+		$category_name = $this->theme;
+		$category_properties = [ 'label' => $category_name ];
+		register_block_pattern_category( $category_name, $category_properties );
+	}
+
+	function register_pattern() {
+		$this->load_pattern();
+		register_block_pattern( $this->pattern_name, $this->pattern_properties );
+	}
+
+}
---
+++ oik-patterns.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+Plugin Name: oik-patterns
+Plugin URI: https://www.oik-plugins.com/oik-plugins/oik-patterns
+Description: Patterns for the Gutenberg block editor
+Version: 0.0.0
+Author: bobbingwide
+Author URI: https://bobbingwide.com/about-bobbing-wde
+Text Domain: oik-patterns
+Domain Path: /languages/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+    Copyright 2020-201 Bobbing Wide (email : herb@bobbingwide.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+/**
+ * We're dependent upon oik
+ */
+function oik_patterns_loaded() {
+	add_action( 'oik_loaded', 'oik_patterns_init', 20 );
+}
+
+function oik_patterns_init() {
+	//oik_require( 'patterns/index.php', 'oik-patterns');
+	//oik_blocks_lazy_register_block_patterns();
+
+	oik_require( 'libs/class-oik-patterns-from-htm.php', 'oik-patterns');
+	$oik_patterns = new OIK_Patterns_From_Htm();
+	$oik_patterns->register_patterns();
+
+}
+
+oik_patterns_loaded();
